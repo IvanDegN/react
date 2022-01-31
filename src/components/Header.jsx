7eaff1fd38e0bfef4b9cdd9ff963b97d827379b1
IvanDegN <mauzer.bruno@yandex.ru>
2022-01-31 20:41:53 +0300

    Разбиение на компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () =>
+{
+    return (
+
+        <header className='header'>
+            <img src='https://cdn.logo.com/hotlink-ok/logo-social.png' alt='logo'/>
+        </header>
+    );
+}
+
+export  default Header;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+const NavBar = () =>
+{
+    return (
+        <nav className='nav'>
+            <ul>
+                <li><a href='#'>Profile</a></li>
+                <li><a href='#'>Messages</a></li>
+                <li><a href='#'>News</a></li>
+                <li><a href='#'>Music</a></li>
+                <br />
+                <br />
+                <li><a href='#'>Settings</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
