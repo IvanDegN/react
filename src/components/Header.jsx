2bf8c8823275ccf939720ca0d01ed6cbc22788b9
IvanDegN <mauzer.bruno@yandex.ru>
2022-01-31 23:30:27 +0300

    Создание css-модулей

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-
+import './Header.module.css';
+import HeaderModuleCss from './Header.module.css';
 
 const Header = () =>
 {
     return (
 
-        <header className='header'>
+        <header className={HeaderModuleCss.header}>
             <img src='https://cdn.logo.com/hotlink-ok/logo-social.png' alt='logo'/>
         </header>
     );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,18 @@
+import './NavBar.module.css';
+import NavBarCssModule from './NavBar.module.css'
+
 const NavBar = () =>
 {
     return (
-        <nav className='nav'>
+        <nav className={NavBarCssModule.nav}>
             <ul>
-                <li><a href='#'>Profile</a></li>
-                <li><a href='#'>Messages</a></li>
-                <li><a href='#'>News</a></li>
-                <li><a href='#'>Music</a></li>
+                <li className={NavBarCssModule.item}><a className={`${NavBarCssModule.link} ${NavBarCssModule.active}`} href='#'>Profile</a></li>
+                <li className={NavBarCssModule.item}><a className={NavBarCssModule.link} href='#'>Messages</a></li>
+                <li className={NavBarCssModule.item}><a className={NavBarCssModule.link} href='#'>News</a></li>
+                <li className={NavBarCssModule.item}><a className={NavBarCssModule.link} href='#'>Music</a></li>
                 <br />
                 <br />
-                <li><a href='#'>Settings</a></li>
+                <li className={NavBarCssModule.item}><a className={NavBarCssModule.link} href='#'>Settings</a></li>
             </ul>
         </nav>
     );
